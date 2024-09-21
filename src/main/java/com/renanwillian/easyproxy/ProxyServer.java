f7555b199f50c65050e5bf005db4c15761c03b94
Renan Willian <contato@renanwillian.com>
2024-09-20 21:08:24 -0300

    Add picoli cli interface

--- src/main/java/com/renanwillian/easyproxy/ProxyServer.java
+++ src/main/java/com/renanwillian/easyproxy/ProxyServer.java
@@ -1,2 +1,33 @@
-package com.renanwillian.easyproxy;public class ProxyServer {
+package com.renanwillian.easyproxy;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ProxyServer  {
+
+    private final int port;
+    private final String targetUrl;
+
+    public ProxyServer(int port, String targetUrl) {
+        this.port = port;
+        this.targetUrl = targetUrl;
+    }
+
+    public void start() throws IOException, InterruptedException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new ProxyHandler(targetUrl));
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        server.setExecutor(executor);
+
+        server.start();
+        System.out.println("Proxy server is running on http://localhost:" + port + "/");
+        System.out.println("Proxying to: " + targetUrl);
+
+        Thread.currentThread().join();
+    }
 }
--- src/main/java/com/renanwillian/easyproxy/ProxyServerCLI.java
+++ src/main/java/com/renanwillian/easyproxy/ProxyServerCLI.java
@@ -1,16 +1,13 @@
 package com.renanwillian.easyproxy;
 
-import com.sun.net.httpserver.HttpServer;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.net.InetSocketAddress;
-
 @Command(name = "proxy-server", mixinStandardHelpOptions = true, version = "1.0",
         description = "Starts a reverse proxy server.")
-public class ProxyServer implements Runnable {
+public class ProxyServerCLI implements Runnable {
 
     @Option(names = {"--port"}, description = "The port on which the server will run (default: 8000).")
     private int port = 8000;
@@ -19,24 +16,15 @@ public class ProxyServer implements Runnable {
     private String targetUrl;
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new ProxyServer()).execute(args);
+        int exitCode = new CommandLine(new ProxyServerCLI()).execute(args);
         System.exit(exitCode);
     }
 
     @Override
     public void run() {
         try {
-            // Start the server with the specified port and target URL
-            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-            server.createContext("/", new ProxyHandler(targetUrl));
-            server.setExecutor(null);
+            ProxyServer server = new ProxyServer(port, targetUrl);
             server.start();
-
-            System.out.println("Proxy server is running on http://localhost:" + port + "/");
-            System.out.println("Proxying to: " + targetUrl);
-
-            // Wait for the server to stop
-            Thread.currentThread().join();
         } catch (Exception e) {
             System.err.println("Error starting the server: " + e.getMessage());
             e.printStackTrace();
